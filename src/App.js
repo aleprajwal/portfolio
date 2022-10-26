16243778289c6291cf678c3e4899ee8a4d96620d
aleprajwal <aleprajwal11@gmail.com>
2022-10-26 11:53:11 +1100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import prajwal from "./assets/prajwal.jpg";
+import "./App.css";
+import resume from "./assets/Resume-Prajwal.pdf";
 
 function App() {
+  const changeMode = () =>{
+    let element = document.body;
+    element.classList.toggle("dark-mode");
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <button className="btn" onClick={changeMode}>Dark Mode</button>
       </header>
+        <section className="main-content">
+          <img src={prajwal} alt="Prajwal Ale" />
+          <h1>Hi, I'm Prajwal.</h1>
+          <article>
+          <p>
+            A passionate and ambitious graduate with a Master of Information
+            Technology from Charles Sturt University. I'm looking for a graduate
+            software developer role. I'm a keen software developer with
+            experience in HTML, CSS, Javascript (React), and python programming.
+            Strong grasp of software design and development, database, cloud
+            technology, computer network, and cybersecurity concepts.
+          </p>
+        </article>
+      </section>
+      <div className="solid-line"/>
+      <footer>
+        <ul className="nav-list">
+          <li><a href="mailto:aleprajwal@gmail.com" target="_blank" rel="noopener noreferrer">aleprajwal@gmail.com</a></li>
+          <li><a href="https://github.com/aleprajwal" target="_blank" rel="noopener noreferrer">Github</a></li>
+          <li><a href="https://www.linkedin.com/in/aleprajwal/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+          <li><a href={resume} target="_blank" rel="noopener noreferrer">Resume</a></li>
+        </ul>
+      </footer>
     </div>
   );
 }
